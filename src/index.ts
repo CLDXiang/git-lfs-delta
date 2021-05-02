26e6b413bf4535b145b2cb9a33c14cc33c0cb2d5
CLDXiang <417198670@qq.com>
2021-05-02 15:37:31 +0800

    feat(ls-files): finish command ls-files

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { createCommand } from 'commander'
 import { viewPaths, addPath } from './track'
+import { lsFiles } from './lsFiles'
 import { clean, filterProcess, smudge } from './filter'
 import { prePush } from './hooks'
 
@@ -19,33 +20,64 @@ program
     addPath(path)
   })
 
+// ls-files
+program
+  .command('ls-files [refs...]')
+  .description(
+    'Display paths of Git LFSD files that are found in the tree at the given reference. If no reference is given, scan the currently checked-out branch. If two references are given, the LFS files that are modified between the two references are shown; deletions are not listed.\nAn asterisk (*) after the OID indicates a full object, a minus (-) indicates an LFSD pointer.',
+  )
+  .option(
+    '-l, --long',
+    'Show the entire 64 character OID, instead of just first 10.',
+  )
+  .option(
+    '-s, --size',
+    'Show the size of the LFS object between parenthesis at the end of a line.',
+  )
+  .option(
+    '-d, --debug',
+    'Show as much information as possible about a LFS file. This is intended for manual inspection; the exact format may change at any time.',
+  )
+  .option(
+    '-a, --all',
+    'Inspects the full history of the repository, not the current HEAD (or other provided reference). This will include previous versions of LFS objects that are no longer found in the current tree.',
+  )
+  .option('-n, --name-only', 'Show only the lfs tracked file names.')
+  .action((refs, options) => {
+    lsFiles(refs, options)
+  })
+
 // hooks
-program.command('post-checkout').action(() => {
+program.command('post-checkout', { hidden: true }).action(() => {
   // console.log('post-checkout')
 })
 
-program.command('post-commit').action(() => {
+program.command('post-commit', { hidden: true }).action(() => {
   // console.log('post-commit')
 })
 
-program.command('post-merge').action(() => {
+program.command('post-merge', { hidden: true }).action(() => {
   // console.log('post-merge')
 })
 
-program.command('pre-push <args...>').action((args) => {
+program.command('pre-push <args...>', { hidden: true }).action((args) => {
   prePush(args)
 })
 
 // filter
-program.command('clean <file>').action(async (file: string) => {
-  await clean()
-})
+program
+  .command('clean <file>', { hidden: true })
+  .action(async (file: string) => {
+    await clean()
+  })
 
-program.command('smudge <file>').action(async (file: string) => {
-  await smudge()
-})
+program
+  .command('smudge <file>', { hidden: true })
+  .action(async (file: string) => {
+    await smudge()
+  })
 
-program.command('filter-process').action(async () => {
+program.command('filter-process', { hidden: true }).action(async () => {
   await filterProcess()
 })
 
